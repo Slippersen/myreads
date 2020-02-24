b8d30eef0f142ee1c4dfb1929cd79a37431a3fe6
Erik J. Sandal <erik.sandal@bouvet.no>
2020-02-24 21:27:51 +0100

    Separate search page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Link } from 'react-router-dom';
-// import * as BooksAPI from './BooksAPI'
+// import * as BooksAPI from './services/BooksAPI';
+import SearchPage from './pages/SearchPage';
 import './App.css';
 
 const App = () => {
@@ -236,32 +237,7 @@ const App = () => {
           </div>
         )}
       />
-      <Route
-        path="/search"
-        render={() => (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <Link to="/">
-                <button className="close-search">Close</button>
-              </Link>
-              <div className="search-books-input-wrapper">
-                {/*
-                NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                You can find these search terms here:
-                https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                you don't find a specific author or title. Every search is limited by search terms.
-              */}
-                <input type="text" placeholder="Search by title or author" />
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-        )}
-      />
+      <Route path="/search" component={SearchPage} />
     </div>
   );
 };
