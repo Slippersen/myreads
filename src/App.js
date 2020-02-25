1b05bc504e917e3788e8879a5be041cc45d30af6
Erik J. Sandal <erik.sandal@bouvet.no>
2020-02-25 20:33:38 +0100

    Add separate dashboard page

--- src/App.js
+++ src/App.js
@@ -1,53 +1,13 @@
-import React, { useContext } from 'react';
-import { Route, Link } from 'react-router-dom';
-import CollectionContext from './contexts/CollectionContext';
+import React from 'react';
+import { Route } from 'react-router-dom';
 import SearchPage from './pages/SearchPage';
-import BooksGrid from './components/BooksGrid';
+import DashboardPage from './pages/DashboardPage';
 import './App.css';
 
 const App = () => {
-  const { collection } = useContext(CollectionContext);
-
   return (
     <div className="app">
-      <Route
-        exact
-        path="/"
-        render={() => (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Currently Reading</h2>
-                  <div className="bookshelf-books">
-                    <BooksGrid books={collection} shelf="currentlyReading" />
-                  </div>
-                </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Want to Read</h2>
-                  <div className="bookshelf-books">
-                    <BooksGrid books={collection} shelf="wantToRead" />
-                  </div>
-                </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Read</h2>
-                  <div className="bookshelf-books">
-                    <BooksGrid books={collection} shelf="read" />
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="open-search">
-              <Link to="/search">
-                <button>Add a book</button>
-              </Link>
-            </div>
-          </div>
-        )}
-      />
+      <Route exact path="/" component={DashboardPage} />
       <Route path="/search" component={SearchPage} />
     </div>
   );
